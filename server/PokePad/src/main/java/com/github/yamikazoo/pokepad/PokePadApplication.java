26fd6311aac37c95718508a2051a18109ee978fc
dyw7 <dyw7@sfu.ca>
2025-12-18 23:59:52 -0800

    initial server commit

---
+++ server/PokePad/src/main/java/com/github/yamikazoo/pokepad/PokePadApplication.java
@@ -0,0 +1,13 @@
+package com.github.yamikazoo.pokepad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokePadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokePadApplication.class, args);
+	}
+
+}
---
+++ server/PokePad/src/test/java/com/github/yamikazoo/pokepad/PokePadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yamikazoo.pokepad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokePadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
