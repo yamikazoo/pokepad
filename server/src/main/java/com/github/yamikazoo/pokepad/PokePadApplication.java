9a37c94e55eb0e605813574e5298f4963bbfc558
dyw7 <dyw7@sfu.ca>
2025-12-21 01:09:59 -0800

    feature: initial db setup

--- server/src/main/java/com/github/yamikazoo/pokepad/PokePadApplication.java
+++ server/src/main/java/com/github/yamikazoo/pokepad/PokePadApplication.java
@@ -2,11 +2,13 @@ package com.github.yamikazoo.pokepad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class PokePadApplication {
 
 	public static void main(String[] args) {
+		Dotenv.configure().ignoreIfMissing().load();
 		SpringApplication.run(PokePadApplication.class, args);
 	}
 
