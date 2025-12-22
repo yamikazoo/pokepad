552d2f5133f35fc77a2f536b2f31212d8e66f6b7
dyw7 <dyw7@sfu.ca>
2025-12-22 01:53:29 -0800

    created initial database schema

--- server/src/main/java/com/github/yamikazoo/pokepad/models/Card.java
+++ server/src/main/java/com/github/yamikazoo/pokepad/models/Card.java
@@ -1,13 +1,19 @@
-package com.github.yamikazoo.pokepad.models; // Change this to match your project path
+package com.github.yamikazoo.pokepad.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Data; // If you are using Lombok
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToMany;
+import java.util.List;
+import lombok.Data;
+import lombok.ToString;
 
 @Entity
-@Data // This Lombok annotation automatically creates Getters and Setters
+@Data // Lombok annotation that automatically creates getters and setters removing the need to manually code them
+@Table(name = "cards")
 public class Card {
     
     @Id
@@ -16,6 +22,17 @@ public class Card {
     
     private String name;
     private String setId;
+
+    @Column(length = 500) // account for long image urls
     private String imageUrl;
+
     private String language; // "EN" or "JP"
+
+    private String artist;
+    private String rarity;
+
+    // creates a many to many foreign key relationship with Collection
+    @ToString.Exclude // avoid stack overflow from circular referencing of collections and cards
+    @ManyToMany(mappedBy = "cards")
+    private List<Collection> collections;
 }
